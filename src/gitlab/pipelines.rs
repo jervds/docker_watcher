18ac99466d58e7e77b2efd1905db8da37359cecc
Jérôme Vanderstichelen <jerome.vanderstichelen@soprabanking.com>
2021-11-26 10:56:31 +0100

    chore: use std:env to retrieve gitlab token

--- src/gitlab/pipelines.rs
+++ src/gitlab/pipelines.rs
@@ -1,5 +1,5 @@
 use serde::{Deserialize, Serialize};
-use std::{error, process};
+use std::{env, error, process};
 
 
 type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
@@ -8,23 +8,27 @@ pub fn get_last_pipeline_run_time(project: String, branch: String) -> Result<Str
     //TODO handle the pagination
     //curl --head --header "PRIVATE-TOKEN: <your_access_token>" "https://gitlab.example.com/api/v4/projects/9/issues/8/notes?per_page=3&page=2"
     //TODO handle the case in which no date is returned
-    let api_url = format!("https://innersource.soprasteria.com/api/v4/{}/pipelines/",project);
-    println!("Querying on: {}",api_url);
+    let gitlab_config = load_gitlab_config();
+    let api_url = format!("https://innersource.soprasteria.com/api/v4/projects/{}/pipelines/", project);
+    println!("Querying on: {}", api_url);
 
-    let pipeline = reqwest::blocking::get(api_url)
-        .unwrap_or_else(|err|{
+    let client = reqwest::blocking::Client::new();
+
+    let pipeline = client.get(api_url).bearer_auth(gitlab_config.token).send()
+        .unwrap_or_else(|err| {
             eprintln!("Error when calling: {}", err);
             process::exit(1);
         })
         .text()
-        .unwrap_or_else(|err|{
+        .unwrap_or_else(|err| {
             eprintln!("Error when calling: {}", err);
             process::exit(2);
         })
         .parse_json()
         .pipelines.into_iter()
         .find(|pipeline| pipeline.r#ref == branch)
-        .unwrap_or_else(||{
+        // .find(|pipeline| pipeline.status == "success")
+        .unwrap_or_else(|| {
             eprintln!("No pipeline found for master!");
             process::exit(3);
         })
@@ -33,27 +37,47 @@ pub fn get_last_pipeline_run_time(project: String, branch: String) -> Result<Str
     Ok(pipeline.created_at.clone())
 }
 
+fn load_gitlab_config() -> GitlabConfig {
+    let cfg = GitlabConfig {
+        token: env::var("GITLAB_TOKEN")
+            .unwrap_or_else(|err| {
+                eprintln!("No token found. Please define GITLAB_TOKEN environment variable - {}",err);
+                process::exit(4);
+            })
+    };
+
+    cfg
+}
+
 trait ParseJson {
     fn parse_json(&self) -> GitlabPipelines;
 }
 
 impl ParseJson for String {
     fn parse_json(&self) -> GitlabPipelines {
-        serde_json::from_str(&*self ).unwrap_or_else(|err|{
+        let pipelines_from_json: Vec<GitlabPipelineApiDescription> = serde_json::from_str(&*self).unwrap_or_else(|err| {
             eprintln!("Error when parsing response: {} on {}", err, self);
             process::exit(1);
-        })
+        });
+        GitlabPipelines{
+            pipelines: pipelines_from_json,
+        }
     }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 struct GitlabPipelines {
-    pipelines: Vec<GitlabPipelineApiDescription>
+    pipelines: Vec<GitlabPipelineApiDescription>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 struct GitlabPipelineApiDescription {
     id: i32,
     created_at: String,
-    r#ref: String
+    status: String,
+    r#ref: String,
+}
+
+struct GitlabConfig {
+    token: String,
 }
