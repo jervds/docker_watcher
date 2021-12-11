f96e0d735966159f0b598656d51ae556d2468aae
Jérôme Vanderstichelen <jerome.vanderstichelen@soprabanking.com>
2021-12-11 16:49:55 +0100

    test: gitlab config

--- src/config/gitlab.rs
+++ src/config/gitlab.rs
@@ -21,3 +21,38 @@ impl GitlabConfig {
     }
 
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn pipeline_api_should_return_valid_url() {
+        let url = "https://innersource.soprasteria.com/api/v4/projects/123456/pipelines/";
+        assert_eq!(GitlabConfig::pipeline_api("123456".to_string()),url)
+    }
+
+    #[test]
+    fn load_config_should_load_gitlab_token_and_return_some() {
+        let key = "GITLAB_TOKEN";
+        env::set_var(key,"123456");
+        assert_eq!(GitlabConfig::load().is_some(),true)
+    }
+
+    #[test]
+    fn load_config_should_load_gitlab_token_and_return_token() {
+        let key = "GITLAB_TOKEN";
+        env::set_var(key,"123456");
+        assert_eq!(GitlabConfig::load().unwrap().token,"123456")
+    }
+
+    #[test]
+    fn load_config_should_return_none_when_env_var_for_gitlab_token_is_not_defined() {
+        // if environment variable is not set, the remove_var can panic.
+        let key = "GITLAB_TOKEN";
+        env::set_var(key,"123456");
+        env::remove_var(key);
+        assert_eq!(GitlabConfig::load().is_none(),true)
+    }
+
+}
